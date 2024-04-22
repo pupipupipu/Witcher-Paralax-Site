b972ab79e57bf29bd38db03e9c483af5a3121028
Andrey Borodin <andreyborodin2000@yandex.ru>
2024-04-22 13:54:48 +0300

    1

---
+++ js/app.js
@@ -0,0 +1,11 @@
+new Swiper('.slider', {
+	// direction: 'vertical',
+	speed: 2400,
+	mousewheel: {
+		enabled: true,
+		sensitivity: 7
+	},
+	spaceBetween: 18,
+	parallax: true,
+	freeMode: true,
+});
